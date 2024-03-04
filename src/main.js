ee809007d2d265730589d8468a83178c8bb9d040
PrettyLolli <yunghunters2015@gmail.com>
2024-03-05 02:02:02 +0300

    finall

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.config.devtools = true;
+
+//console.log(Vue.config.devtools);
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
